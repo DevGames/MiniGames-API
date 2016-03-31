c537770f7a7608ddc6082ef10e7917f3d3b6eae6
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-03-31 21:26:04 +0300

    Update Teams.php

--- src/MiniGamesTree/Teams.php
+++ src/MiniGamesTree/Teams.php
@@ -4,8 +4,25 @@ namespace MiniGamesTree;
 
 class Teams extends Base\ArrayBase{
   
+  public $gts;
+  
   public function addTeam($teamsname,$name){
   return $this->worker[$teamsname]{$name} = $name;
   }
   
+  public function getScore(){
+  $this->gts = new teamscore();
+  return $this->gts;
+  }
+  
+}
+class teamscore extends Base\ArrayBase{
+  
+  public function getScoreTeam(){
+    
+  }
+  
+  public function setScoreTeam(){
+    
+  }
 }

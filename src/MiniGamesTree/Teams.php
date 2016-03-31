6e331f575efb43b182c2bbc0e64ddd507e85c456
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-03-31 22:07:05 +0300

    Add Teams Function

--- src/MiniGamesTree/Teams.php
+++ src/MiniGamesTree/Teams.php
@@ -6,8 +6,8 @@ class Teams extends Base\ArrayBase{
   
   public $gts;
   
-  public function addTeam($teamsname,$name){
-  return $this->worker[$teamsname]{$name} = $name;
+  public function addToTeam($teamsname,$name){
+  return $this->worker[strtoupper($teamsname)]{$name} = $name;
   }
   
   public function getScore(){
@@ -15,29 +15,45 @@ class Teams extends Base\ArrayBase{
   return $this->gts;
   }
   
+  public function getAllPlayersInTeamName($teamname,$gule = " : "){
+    return join($gule,$this->worker{strtoupper($teamname)});
+  }
+  
   public function isPlayersInGame($player){
     return (in_array($player->getName(),$this->worker));
   }
   
   public function getTeamInGame($teamname,$player){
-    foreach($this->worker{$teamname} as $teams){
+    foreach($this->worker{strtoupper($teamname)} as $teams){
       return ($teams == $player->getName());
     }
   }
   
-  public function getTeamsPlayer($teamname,$server){
-    foreach($this->worker{$teamname} as $teams){
+  public function getPlayersTeams($teamname,$server){
+    foreach($this->worker{strtoupper($teamname)} as $teams){
       return $server->getPlayer($teams);
     }
+  }
+  
+  public function setTeamWon($teamname){
+    return $this->won = strtoupper($teamname);
+  }
+  
+  public function getTeamWon(){
+  return $this->won;
+  }
+  
+  public function getCountTeam($teamname){
+  return count($this->worker[strtoupper($teamname)]);
   }
 }
 class teamscore extends Base\ArrayBase{
   
   public function getScoreTeam($teamname){
-  return $this->teamscore[$teamname];
+  return $this->teamscore[strtoupper($teamname)];
   }
   
   public function setScoreTeam($teamname,$amount){
-  return $this->teamscore[$teamname] = $amount;
+  return $this->teamscore[strtoupper($teamname)] = $amount;
   }
 }

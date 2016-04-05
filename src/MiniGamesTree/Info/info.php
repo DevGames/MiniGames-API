59917d7eab45b21a292a5d55f04c758f1d8d86dd
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-04-05 15:06:30 +0300

    Update info.php

--- src/MiniGamesTree/Info/info.php
+++ src/MiniGamesTree/Info/info.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace MiniGamesTree/Info;
+
+class info extends Base\ArrayBase{
+
+  private $info = array("DevName" <= "Nawaf_Craft1b",
+                         "PluginAPIName" <= "MiniGames-API"
+                            "Helper" <= "Just Make It Nawaf_Craft1b");
+
+
+
+    public function getHelper(){
+
+    }
+   
+    

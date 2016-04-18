78474e52eecd5f15390d962ce511c6a58d307c74
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-04-18 17:21:07 +0300

    Update PlayerScore.php

--- src/MiniGamesTree/Score/PlayerScore.php
+++ src/MiniGamesTree/Score/PlayerScore.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace MiniGamesTree\Score;
+
+class PlayerScore extends \MiniGamesTree\Singles{
+    
+    public $score = array();
+    
+    public $eror = "";
+
+    public function setScore($name,$amount){
+        if($this->isPlayer($name)){
+            $this->score[$name] = $amount;
+        }else{
+            unset($this->score{$name});
+            $output = $name." IS Not In Game , Canot Set Score";
+            $output .= " ";
+            {$this->eror = $output;}
+        }
+    }
+    
+    public function getScore($name){
+        if($this->isPlayer($name)){
+            $this->score[$name];
+        }else{
+            $output = $name." IS Not In Game , Canot get Score";
+            $output .= " ";
+            {$this->eror = $output;}
+        }
+    }
+    
+    public function getError(){
+    return $this->eror;
+    }
+}

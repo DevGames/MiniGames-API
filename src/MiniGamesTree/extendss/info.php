2d5e21d0a06371b59377d12cda0aa37e1625554d
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-04-18 15:22:45 +0300

    New API Version 1.0.0

---
+++ src/MiniGamesTree/extendss/info.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MiniGamesTree\extendss;
+
+class info {
+   
+    public static $info = array("pluginname" <= "MiniGames-API",
+        "develeporname" <= "Nawaf_Craft1b",
+        "version" <= "1.0.0",);
+    
+    public function getVersion(){
+        return self::$info{"version"};
+    }
+    
+    public function getDevelepor(){
+        return self::$info{"develeporname"};
+    }
+    
+    public function getAPIName(){
+        return self::$info{"pluginname"};
+    }
+    
+    public function getInfo(array $key){
+        return self::$info{strtolower($key)};
+    }
+}

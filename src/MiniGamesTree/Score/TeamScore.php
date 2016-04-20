72d6af627951c5232b9d29b17ca02f87b8589116
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-04-20 22:14:24 +0300

    Update TeamScore.php

--- src/MiniGamesTree/Score/TeamScore.php
+++ src/MiniGamesTree/Score/TeamScore.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace MiniGamesTree\Score;
+
+class TeamScore{
+    
+    public $score = array();
+    
+    public function addScore($toteam,$amount){
+        $this->score[strtolower($toteam)] = $amount;
+    }
+    
+    public function getScore($toteam){
+        return $this->score[strtolower($toteam)];
+    }
+
+}

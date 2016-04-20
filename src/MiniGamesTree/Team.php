8c669f4b87474113f4d46742415270a633dc005e
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-04-20 16:08:35 +0300

    Update Team.php

--- src/MiniGamesTree/Team.php
+++ src/MiniGamesTree/Team.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace MiniGamesTree;
+
+class Team {
+  
+  private $teams = array();
+  
+  public function addTo($teamname,$name){
+    return $this->teams[strtolower($teamname)][] = $name;
+  }
+  
+  public function RemoveTo($teamname,$name){
+   unset($this->teams[array_search($name, $this->teams[strtolower($teamname]))]);
+  }
+  
+  public function getCount($teamname){
+    return count($this->teams[strtolower($teamname)]);
+  }
+  
+  public function isInTeam($teamname,$name){
+        return in_array($name, $this->teams[strtolower($teamname)]);
+    }
+    
+    public function getPlayersInTeam(\pocketmine\Server $server,$teamname){
+    foreach ($this->teams[strtolower($teamname)] as $p):
+    return $server->getPlayer($p);
+    endforeach;
+    }
+    
+    public function listPlayerTeams($teamname,$glue = " .*. "){
+        return implode($glue, $this->teams[strtolower($teamname)]);
+    }
+  
+}

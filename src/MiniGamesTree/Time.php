989f41aa44f4f5f947029e2b992bb7c0690fb3e7
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-04-20 16:49:23 +0300

    Update Time.php

--- src/MiniGamesTree/Time.php
+++ src/MiniGamesTree/Time.php
@@ -1,1 +1,79 @@
+<?php
 
+namespace MiniGamesTree;
+
+use pocketmine\scheduler\PluginTask;
+
+class Time extends \pocketmine\plugin\PluginBase {
+  
+  private $Time = 0;
+  
+  private static $speed = 20;
+  
+  private $move = "-";
+  
+  private $moveperiod = 1;
+
+  public function getTime(){
+    return $this->Time;
+  }
+  
+  public function getMovePeriod(){
+      return $this->moveperiod;
+  }
+  
+  public function setMovePeriod($amount){
+      return $this->moveperiod = $amount;
+  }
+
+  public function setTime($amount){
+    return $this->Time = $amount + 1;
+  }
+  
+  public function setSpeed($amount){
+    return self::$speed = $amount * (1);
+  }
+  
+  public function setTimeMove($move = "-"){
+    if($move == "-"){
+      $this->move = "-";
+    }
+    if($move == "+"){
+      $this->move = "+";
+    }
+  }
+  
+    public function onEnable() {
+
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      
+      $this->getServer()->getScheduler()->scheduleRepeatingTask(new Run ( $this ), self::$speed);
+      
+  }
+  
+  public function getMove(){
+      return $this->move;
+  }
+  
+  public function getSpeed(){
+      return self::$speed;
+  }
+}
+class Run extends PluginTask{
+
+        public $run;
+
+	public function __construct(Time $run){
+		parent::__construct($run);
+                $this->run = $run;
+	}
+	
+	public function onRun($currentTick){
+           if($this->run->getMove() == "-"){
+           $this->run->setTime($this->run->getTime() - $this->run->getMovePeriod());
+           }
+           if($this->run->getMove() == "+"){
+           $this->run->setTime($this->run->getTime() + $this->run->getMovePeriod());
+           }
+        }
+}

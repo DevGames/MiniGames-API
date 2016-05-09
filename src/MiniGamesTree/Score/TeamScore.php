769fab53235cb9f3098f19f17aa56fc6fe46411b
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-05-09 19:59:11 +0300

    Update TeamScore.php

--- src/MiniGamesTree/Score/TeamScore.php
+++ src/MiniGamesTree/Score/TeamScore.php
@@ -2,20 +2,20 @@
 
 namespace MiniGamesTree\Score;
 
-class TeamScore{
+class TeamScore extends Team{
     
     public $score = array();
     
-    public function addScore($teamname,$amount){
-        $this->score[strtolower($teamname)] = $amount;
+    public function addScore($amount){
+        $this->score[strtolower($this->ust)] = $amount;
     }
     
-    public function getScore($teamname){
-        return $this->score[strtolower($teamname)];
+    public function getScore(){
+        return $this->score[strtolower($this->ust)];
     }
     
     public function DisableScore($teamname){
-       unset($this->score[$teamname]);
+       unset($this->score[$this->ust]);
     }
 
 }

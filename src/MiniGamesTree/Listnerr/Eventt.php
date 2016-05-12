f51c4f3f13ed318845a8df729ef3b1d132e42b08
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-05-12 13:27:55 +0300

    Create Eventt.php

---
+++ src/MiniGamesTree/Listnerr/Eventt.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pets\Listener;
+
+use pocketmine\event\Listener;
+use MiniGamesTree\Time as Main;
+use pocketmine\event\entity\EntityDamageEvent as Damage;
+
+class Eventt implements Listener {
+
+    private $main;
+    
+    public function __construct(Main $a) {
+        $this->main = $a;
+    }
+    
+    public function onDamage(Damage $event){
+     if ($event instanceof EntityDamageByEntityEvent) {
+            if ($event->getEntity() instanceof Player) {
+    if( $this->main->api->getGame()->getTeam("",$event->getEntity()->getName())->getTeam() == $this->main->api->getGame()->getTeam("",$event->getDamager()->getName())->getTeam() ) {
+    $event->setCancelled(true);
+      }
+     }
+    }
+  }
+    
+    
+}

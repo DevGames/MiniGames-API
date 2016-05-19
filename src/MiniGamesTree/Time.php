97d19620775bd8e5cc74bda241b0b03ec32a1b24
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-05-19 17:00:34 +0300

    Update Time.php

--- src/MiniGamesTree/Time.php
+++ src/MiniGamesTree/Time.php
@@ -47,7 +47,7 @@ class Time extends \pocketmine\plugin\PluginBase {
   
     public function onEnable() {
 
-      $this->getServer()->getPluginManager()->registerEvents(new MiniGamesTree\Listnerr\Eventt($this), $this);
+      $this->getServer()->getPluginManager()->registerEvents(new Event($this), $this);
       
       $api = new \MiniGamesTree\API\game;
       $this->api = $api;

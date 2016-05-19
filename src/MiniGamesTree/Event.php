8a204434aa05ae74bccb36efff6c699e3d0deb23
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-05-19 16:59:46 +0300

    Create Event.php

---
+++ src/MiniGamesTree/Event.php
@@ -0,0 +1,36 @@
+<?php
+namespace MiniGamesTree; # Fixed
+use pocketmine\event\Listener; 
+use MiniGamesTree\Time as Main;
+use pocketmine\event\entity\EntityDamageEvent as Damage;
+class Eventt implements Listener {
+    private $main;
+    
+    public function __construct(Main $a) {
+        $this->main = $a;
+    }
+    
+    public function onDamage(Damage $event){
+        
+        $b = 0;
+        
+     if ($event instanceof EntityDamageByEntityEvent) {
+         
+    if ($event->getEntity() instanceof Player) {
+        
+    if( $this->main->api->getGame()->getTeam("",$event->getEntity()->getName())->getTeam() 
+    == $this->main->api->getGame()->getTeam("",$event->getDamager()->getName())->getTeam() ) {
+        
+        if( $b == ( 0 ) ){
+        
+    $event->setCancelled(true);
+    
+     }
+     
+    $this->main->getServer()->getPluginManager()->callEvent(new \MiniGamesTree\GameEvent\Team\TeamDamageEvent($event->getEntity(), $event->getDamager(), $b , $this->main->api->getGame()->getTeam("",$event->getDamager()->getName())->getTeam() , $this->main->api->getGame()->getTeam("",$event->getEntity()->getName())->getTeam()));
+    
+    }
+  }
+}
+    
+}

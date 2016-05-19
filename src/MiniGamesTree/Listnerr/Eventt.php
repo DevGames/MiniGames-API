f065be99c67cfd57a5e67827baeb35ef1433afb3
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-05-19 15:16:33 +0300

    Finished New Version

--- src/MiniGamesTree/Listnerr/Eventt.php
+++ src/MiniGamesTree/Listnerr/Eventt.php
@@ -15,13 +15,26 @@ class Eventt implements Listener {
     }
     
     public function onDamage(Damage $event){
+        
+        $b = 0;
+        
      if ($event instanceof EntityDamageByEntityEvent) {
-            if ($event->getEntity() instanceof Player) {
-    if( $this->main->api->getGame()->getTeam("",$event->getEntity()->getName())->getTeam() == $this->main->api->getGame()->getTeam("",$event->getDamager()->getName())->getTeam() ) {
+         
+    if ($event->getEntity() instanceof Player) {
+        
+    if( $this->main->api->getGame()->getTeam("",$event->getEntity()->getName())->getTeam() 
+    == $this->main->api->getGame()->getTeam("",$event->getDamager()->getName())->getTeam() ) {
+        
+        if( $b == ( 0 ) ){
+        
     $event->setCancelled(true);
+    
      }
+     
+    $this->main->getServer()->getPluginManager()->callEvent(new \MiniGamesTree\GameEvent\Team\TeamDamageEvent($event->getEntity(), $event->getDamager(), $b , $this->main->api->getGame()->getTeam("",$event->getDamager()->getName())->getTeam() , $this->main->api->getGame()->getTeam("",$event->getEntity()->getName())->getTeam()));
+    
     }
   }
-    }
+}
     
 }

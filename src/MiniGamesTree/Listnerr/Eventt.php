c2f09bd228ebad283dc3d191de0003c4c2ea37fa
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-05-19 08:56:13 +0300

    Update Eventt.php

--- src/MiniGamesTree/Listnerr/Eventt.php
+++ src/MiniGamesTree/Listnerr/Eventt.php
@@ -17,15 +17,11 @@ class Eventt implements Listener {
     public function onDamage(Damage $event){
      if ($event instanceof EntityDamageByEntityEvent) {
             if ($event->getEntity() instanceof Player) {
-                $main = \MiniGamesTree\privatee\BoolEvent();
-                if($main->teamdamage == 1){
     if( $this->main->api->getGame()->getTeam("",$event->getEntity()->getName())->getTeam() == $this->main->api->getGame()->getTeam("",$event->getDamager()->getName())->getTeam() ) {
     $event->setCancelled(true);
-      }
-                }
      }
     }
   }
-    
+    }
     
 }

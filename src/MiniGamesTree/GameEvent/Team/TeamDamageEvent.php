f5d6f9d88b70ed77c5b6975708026d5c46516108
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-05-19 15:03:10 +0300

    Update TeamDamageEvent.php

--- src/MiniGamesTree/GameEvent/Team/TeamDamageEvent.php
+++ src/MiniGamesTree/GameEvent/Team/TeamDamageEvent.php
@@ -1,1 +1,34 @@
-# soon
+<?php
+
+namespace MiniGamesTree/GameEvent/Team;
+
+use pocketmine\event\Cancellable;
+
+class TeamDamageEvent implements Cancellable{
+
+    private $player;
+    private $damager;
+    private $bool;
+    
+    public function __construct($player , $damager , $bool){
+        $this->player = $player;
+        $this->damager = $damager;
+        $this->bool = $bool;
+    }
+    
+    public function getEntity(){
+        return $this->player;
+    }
+    
+    public function getDamager(){
+        return $this->player;
+    }
+    
+    public function setDamageEnabled($bool = true) {
+      if( $bool == true ) {
+        $this->bool = 0;
+          }  else  {
+            $this->bool = 1;
+          }
+    }
+}
